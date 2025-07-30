dde7d87ecb472d2cfa939dae1b0fe02744980b41
Вадим <vadim@MBP-Vadim.NEOTECH>
2025-07-30 17:50:45 +0500

    micro in progress 2

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -48,12 +48,19 @@ func main() {
 		Models: data.New(client),
 	}
 
+	// start the server
+	go app.serve()
 }
 
 func (app *Config) serve() {
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
 	}
 }
 
