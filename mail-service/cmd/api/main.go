459bb58724fa840db1f88dc92cf6add68681889e
Вадим <vadim@MBP-Vadim.NEOTECH>
2025-07-31 11:52:25 +0500

    erhhrhjyjj

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -6,22 +6,21 @@ import (
 	"net/http"
 )
 
-const webPort = "80"
+type Config struct {
+}
 
-type Config struct {}
+const webPort = "80"
 
 func main() {
 	app := Config{}
 
-	log.Printf("Starting broker service on port %s", webPort)
+	log.Println("Starting mail service on port", webPort)
 
-	// define http server
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%s", webPort),
+		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
-	// start the server
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
